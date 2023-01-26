5ec32bf584be01a73ab1f55a4ed6ac6af3da9564
Priyanshu <Priyanshujoshi2020@gmail.com>
2023-01-26 23:55:48 +0530

    completed this ques

--- Ques11-12.js
+++ Ques11-12.js
@@ -11,10 +11,18 @@ let year = date.getFullYear()
 
 let hour = date.getHours()
 
+let minute = date.getMinutes()
+
+let seconds = Date.parse("January 26, 2023")* (0.001)
+
+
 console.log(day);
 console.log(month);
 console.log(year);
 console.log(` Today's date is ${day}/${month}/${year}`);
 console.log("Hour-" + hour)
+console.log("Minutes" + minute);
+
+console.log(" Total seconds since 1970 are - " + seconds)
 
 
